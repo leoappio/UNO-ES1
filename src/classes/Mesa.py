854f0979388581fdcd451f7f8cfe067701a2bc71
Leonardo <leonardo.appio@gmail.com>
2022-12-05 01:23:24 -0300

    ble

--- src/classes/Mesa.py
+++ src/classes/Mesa.py
@@ -16,7 +16,7 @@ class Mesa:
             self.cor_atual = cor
             
     def set_carta_atual(self, carta):
-        self.baralho.cartas.append(self.carta_atual)
+        self.baralho.cartas.insert(0, self.carta_atual)
         self.carta_atual = carta
 
     def get_carta_atual(self):
